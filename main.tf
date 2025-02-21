f2e05fa6415528d5463ae57c61806a315dccb18e
Mirabel Eyong <mirabel_eyong@yahoo.com>
2025-02-21 12:55:53 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+# resource "aws_instance" "web" {
+#   ami           = "ami-0e1bed4f06a3b463d"
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "HelloWorld"
+#   }
+# }
+
+
+
+
 resource "aws_instance" "web" {
   ami           = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
+
   tags = {
     Name = "HelloWorld"
   }
