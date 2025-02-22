d682fe1c2599d3761cb5820dc4ded6087b650111
Mirabel Eyong <mirabel_eyong@yahoo.com>
2025-02-22 13:35:51 +0100

    updated

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
-# resource "aws_instance" "web" {
-#   ami           = "ami-0e1bed4f06a3b463d"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "HelloWorld"
-#   }
-# }
-
-
-
-
 resource "aws_instance" "web" {
   ami           = "ami-0e1bed4f06a3b463d"
   instance_type = "t2.micro"
-
   tags = {
-    Name = "HelloWorld"
+    Name = "Github-Action-ec2"
   }
 }
+
+
+
+
